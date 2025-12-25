db5fb4875f4c1e5b63d89fee9dfcb0d73e64758d
YousraElmag <yousra3489@gmail.com>
2025-12-25 14:24:07 +0100

    step 2 is done

--- challengenum3/challenge_three.js
+++ challengenum3/challenge_three.js
@@ -8,3 +8,45 @@ for(const char of text){
 for(const[char,count] of frequencyMap){
    console.log(JSON.stringify(char),'=>',count )
 }
+
+class Node {
+  constructor(char, freq, left = null, right = null) {
+    this.char = char; 
+    this.freq = freq;   
+    this.left = left;  
+    this.right = right; 
+  }
+}
+
+
+const nodes = [];
+for (const [char, freq] of frequencyMap) {
+  nodes.push(new Node(char, freq));
+}
+
+while (nodes.length > 1) {
+  nodes.sort((a, b) => a.freq - b.freq);
+
+  const node1 = nodes.shift();
+  const node2 = nodes.shift();
+
+  const merged = new Node(null, node1.freq + node2.freq, node1, node2);
+
+  nodes.push(merged);
+}
+
+const root = nodes[0];
+
+// -----------------------
+function printCodes(node, code = '') {
+  if (!node) return;
+
+
+  if (node.char) {
+    console.log(JSON.stringify(node.char), '=>', code);
+  }
+
+  printCodes(node.left, code + '0');
+  printCodes(node.right, code + '1');
+}
+printCodes(root);
